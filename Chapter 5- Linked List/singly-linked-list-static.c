229b540856854cefd0f7ae707d86d11b3defe713
Shift2OcO <sagarbasnet0007@gmail.com>
2026-02-26 22:13:26 +0545

    added data and traversal of the list
    added data statically and traversed ii

--- Chapter 5- Linked List/singly-linked-list-static.c
+++ Chapter 5- Linked List/singly-linked-list-static.c
@@ -7,9 +7,27 @@ struct Node_single_list {
 
 int main (void){
     node first_node,second_node,third_node,fourth_node;
+    
+    
+    first_node.actual_data=100;
+    second_node.actual_data=200;
+    third_node.actual_data=300;
+    fourth_node.actual_data=400;
+
+
     first_node.pointer2next=&second_node;
     second_node.pointer2next=&third_node;
     third_node.pointer2next=&fourth_node;
     fourth_node.pointer2next=NULL;
+
+    node *temp= &first_node;
+    while (temp !=NULL){
+        printf("%d\n",temp->actual_data);
+        temp=temp->pointer2next;
+    }
+    printf("End of the list.\n");
+
+    return 0;
+  
     
 }
